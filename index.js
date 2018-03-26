3f68beb18884548bc35872a93046550968732a1a
Sonia  D <sduarte22@gmail.com>
2018-03-26 19:06:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,49 +17,67 @@ var gameInterval = null
  * but all of your work should happen below.
  */
 
-function checkCollision(rock) {
-  // implement me!
-  // use the comments below to guide you!
-  const top = positionToInteger(rock.style.top)
+// function checkCollision(rock) {
+//   // implement me!
+//   // use the comments below to guide you!
+//   const top = positionToInteger(rock.style.top)
 
-  // rocks are 20px high
-  // DODGER is 20px high
-  // GAME_HEIGHT - 20 - 20 = 360px;
-  if (top > 360) {
-    const dodgerLeftEdge = positionToInteger(DODGER.style.left)
+//   // rocks are 20px high
+//   // DODGER is 20px high
+//   // GAME_HEIGHT - 20 - 20 = 360px;
+//   if (top > 360) {
+//     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = positionToInteger(DODGER.style.right);
+//     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
+//     const dodgerRightEdge = positionToInteger(DODGER.style.right);
 
-    const rockLeftEdge = positionToInteger(rock.style.left)
+//     const rockLeftEdge = positionToInteger(rock.style.left)
 
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = positionToInteger(DODGER.style.right)
+//     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
+//     const rockRightEdge = positionToInteger(DODGER.style.right)
     
-  if ((rockLeftEdge<dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) || 
-     (rockLeftEdge>dodgerLeftEdge && rockRightEdge < dodgerRightEdge) ||
-     (rockLeftEdge<dodgerRightEdge && rockRightEdge >dodgerRightEdge)){
+//   if ((rockLeftEdge<dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) || 
+//     (rockLeftEdge>dodgerLeftEdge && rockRightEdge < dodgerRightEdge) ||
+//     (rockLeftEdge<dodgerRightEdge && rockRightEdge >dodgerRightEdge)){
        
-       /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */
-      return false;
+//       /**
+//               * Think about it -- what's happening here?
+//               * There's been a collision if one of three things is true:
+//               * 1. The rock's left edge is < the DODGER's left edge,
+//               *    and the rock's right edge is > the DODGER's left edge;
+//               * 2. The rock's left edge is > the DODGER's left edge,
+//               *    and the rock's right edge is < the DODGER's right edge;
+//               * 3. The rock's left edge is < the DODGER's right edge,
+//               *    and the rock's right edge is > the DODGER's right edge
+//               */
+//       return false;
    
-    } else {
-      return true;
-    }
+//     } else {
+//       return true;
+//     }
      
                
 
-}
+// }
   
+// }
+
+function checkCollision(rock) {
+  const top = positionToInteger(rock.style.top)
+  if (top > 360) {
+    const dodgerLeftEdge = positionToInteger(DODGER.style.left)
+    const dodgerRightEdge = dodgerLeftEdge + 40;
+
+    const rockLeftEdge = positionToInteger(rock.style.left)
+    const rockRightEdge = rockLeftEdge + 20;
+
+    if (
+      (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
+      (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
+      (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)) {
+        return true
+      }
+  }
 }
 
 function createRock(x) {
